5c879eea7d2ab5497a77728617e1de721a9803b8
Istvan078 <kalmaristvan078@gmail.com>
2023-12-02 18:49:14 +0100

    auth

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,14 +11,6 @@ app.listen(port,() => {
     console.log(`A port a ${port}-on fut.`)
 })
 
-app.use((req, res, next) => {
-    if(req.method) {
-        res.status(503).send("A szolgáltatás karbantartás alatt van, ideiglenesen nem elérhető")
-    } else {
-        next()
-    }
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const User = require("../models/user")
+const auth = require("../middleware/auth")
 const router = new express.Router()
 
-router.post("/users", async (req, res) => {
+router.post("/users" , async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
@@ -23,13 +24,30 @@ router.post("/users/login", async (req, res) => {
     }
 })
 
-router.get("/users", async (req,res) => {
+router.post("/users/logout", auth, async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(err) {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.status(200).send()
+    } catch (err) {
         res.status(500).send()
     }
+})
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.status(200).send()
+    } catch (err) {
+        res.status(500).send()
+    }
+})
+
+router.get("/users/me", auth, async (req,res) => {
+    res.send(req.user)
 })
 
 router.get("/users/:id", async (req,res) => {
